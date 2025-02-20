882d024624aa6312949e38fd8ce766e7bea13211
Gnaneswari <gnlolugu@iu.edu>
2025-02-20 08:57:39 -0500

    first commit

--- public/script.js
+++ public/script.js
@@ -1,56 +1,53 @@
 const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
+  host:'/',
   port: '3001'
 })
-const myVideo = document.createElement('video')
+const videoGrid = document.getElementById("video-grid")
+const myVideo = document.createElement('video');
 myVideo.muted = true
-const peers = {}
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
-}).then(stream => {
+}).then(stream =>{
   addVideoStream(myVideo, stream)
 
-  myPeer.on('call', call => {
+  myPeer.on('call', call =>{
     call.answer(stream)
     const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
+    // setTimeout(5000);
+    call.on('stream', userVideoStream=>{
       addVideoStream(video, userVideoStream)
     })
   })
-
-  socket.on('user-connected', userId => {
+  socket.on('user-connected', userId =>{
     connectToNewUser(userId, stream)
+    setTimeout(connectToNewUser,1000,userId,stream)
   })
 })
 
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
-
-myPeer.on('open', id => {
+myPeer.on('open', id=>{
   socket.emit('join-room', ROOM_ID, id)
 })
 
-function connectToNewUser(userId, stream) {
+function connectToNewUser(userId, stream){
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
+  call.on('stream', userVideoStream =>{
     addVideoStream(video, userVideoStream)
   })
-  call.on('close', () => {
+  call.on('close', ()=>{
     video.remove()
   })
-
-  peers[userId] = call
 }
 
-function addVideoStream(video, stream) {
+function addVideoStream(video,stream){
   video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
+  video.addEventListener('loadedmetadata', ()=>{
     video.play()
   })
   videoGrid.append(video)
+  
+  // console.log(video.srcObject)
 }
